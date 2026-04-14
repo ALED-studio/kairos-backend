d0b6b295d663794c04f48e15e852ade55a3d6e9a
Christophe Sahid <christophe.sahid@pm.me>
2026-04-14 14:49:57 +0200

    chore: init project with spring initializr

---
+++ src/test/java/com/github/kairos/KairosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kairos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KairosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
